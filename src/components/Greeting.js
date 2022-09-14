53f057cae8f56369327bc999e408ce7d746f7c2b
Rafael Guzman <rega1237@gmail.com>
2022-09-13 21:44:56 -0400

    add reducer and store and call to the api

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMessage } from '../redux/reducer';
 
 const Greeting = () => {
+  const dispatch = useDispatch();
+  const { greeting } = useSelector((state) => state.greet);
+
+  useEffect(() => {
+    dispatch(fetchMessage);
+  }, [dispatch]);
+
   return (
     <div>
       <h1>      
-        Hello, World Greetings!
+        {greeting}
       </h1>
     </div>
   );
